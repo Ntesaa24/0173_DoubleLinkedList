5b34ece7b29bc9cc4875292cce7beb1cdc01ba47
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-05-02 11:18:06 +0700

    step 2 untuk insert

--- Double_linked/Double_linked.cpp
+++ Double_linked/Double_linked.cpp
@@ -20,4 +20,14 @@ void addNode() {
 	cin >> newNode->name;  //Assign value to the data field of the new node 
 
 
+	// Insert the new node in the list 
+	if (START == NULL || newNode->noMhs <= START->noMhs) { // step 2: insert the new node 
+
+		if (START != NULL && newNode->noMhs == START->noMhs) {
+			cout << "\033[31Dumplicate roll numbers not allowed\033[0m" << endl; 
+			return; 
+		}
+
+	}
+
 }
