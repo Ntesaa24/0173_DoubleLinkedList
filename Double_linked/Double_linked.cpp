cb9e45a03424a1eda51e52c324656449ca116339
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-05-03 23:22:53 +0700

    void seacrhData

--- Double_linked/Double_linked.cpp
+++ Double_linked/Double_linked.cpp
@@ -146,3 +146,34 @@ void traverse()
 		}
 	}
 }
+void revtraverse()
+{
+
+	if (listEmpty())
+		cout << "\nRecords in descending order of roll number are:" << endl;
+	else
+	{
+		cout << "\nRecords im descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+		while (currentNode != NULL)
+		{
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+
+	}
+}
+
+void seacrhData()
+{
+	if (listEmpty() == true)
+	{
+		cout << "\nList is empty" << endl;
+	}
+	Node* prev, *curr; 
+	prev = curr = NULL; 
+	cout << "\nEnter the roll number of the student whose record you want to seacrh: "; 
+	int num; 
+}
